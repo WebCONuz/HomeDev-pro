83e11f353357ddbd78c4d6695bea6026963ea72a
WebCONuz <muxammadi0799@gmail.com>
2023-02-07 18:25:21 +0500

    Complete Blog for PC

--- src/js/app.js
+++ src/js/app.js
@@ -11,3 +11,22 @@ owl1.owlCarousel({
     },
   },
 });
+
+const owl2 = $(".blog__slider");
+owl2.owlCarousel({
+  loop: true,
+  nav: true,
+  dots: true,
+  margin: 40,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    640: {
+      items: 2,
+    },
+    1024: {
+      items: 3,
+    },
+  },
+});

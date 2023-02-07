986660d96e840fc69dc4c530a1353eccc43aee6d
WebCONuz <muxammadi0799@gmail.com>
2023-02-07 15:48:58 +0500

    Complete Service for PC

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+// Customer slider -----------------------------
+const owl1 = $(".service__carousel");
+owl1.owlCarousel({
+  loop: true,
+  nav: true,
+  dots: false,
+  margin: 10,
+  responsive: {
+    0: {
+      items: 1,
+    },
+  },
+});

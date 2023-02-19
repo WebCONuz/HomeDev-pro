0620474d25d5b67fe42b16c72f4ff82db95d4842
WebCONuz <muxammadi0799@gmail.com>
2023-02-19 22:29:53 +0500

    Add loader

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,11 @@
+// LOADEING PAGE -----------------
+window.addEventListener("load", loading);
+function loading() {
+  let loader = document.querySelector(".loader");
+  loader.style.cssText = "opacity: 0; visibility: hidden;";
+}
+
+// AFTER lOAD HTML DOCUMENT
 window.addEventListener("DOMContentLoaded", function () {
   function replaceClass(element, delClassArr, addClassArr) {
     element.classList.remove(...delClassArr);
